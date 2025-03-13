107303788db94eb8ad2dc10c75dbdcd5dd62dcf8
DaviZelski <davi.zelski@gmail.com>
2025-03-13 01:12:00 -0300

    Add project files.

---
+++ Enigma-Machine/PlugBoard.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enigma_Machine
+{
+    class PlugBoard
+    {
+        private Dictionary<char, char> _plugboardConnections = new Dictionary<char, char>();
+
+        public void SetupPlugboard()
+        {
+
+            int amountOfLetters = 1;
+            bool acceptedSwap = false;
+            Console.WriteLine("How many letters will be swapped? (in multiples of 2, since the swap is between two letters. Max is 26)");
+            while ((amountOfLetters % 2) != 0 && !acceptedSwap)
+            {
+                amountOfLetters = int.Parse(Console.ReadLine());
+
+                if (amountOfLetters < 2)
+                {
+                    Console.WriteLine("Number too low, minimum is 2!");
+                }
+                else if (amountOfLetters % 2 != 0)
+                {
+                    Console.WriteLine("Number must be a multiple of 2!");
+                }
+                else if (amountOfLetters > 26)
+                {
+                    Console.WriteLine("Number must not be higher than the amount of letters in the alphabet!");
+                }
+                else if (amountOfLetters >= 2 && amountOfLetters <= 26)
+                {
+                    Console.WriteLine($"Swapping {amountOfLetters} letters!");
+
+                    for (int i = 0; i < (amountOfLetters / 2); i++)
+                    {
+                        Console.WriteLine("Please input which letters will be swapped in this format: (OriginalLetter,LetterToSwap)");
+                        string input = Console.ReadLine();
+
+                        input = input.Trim('(', ')');
+                        string[] letters = input.Split(',');
+
+                        char originalLetter = letters[0][0];
+                        char letterToSwap = letters[1][0];
+
+                        Console.WriteLine($"Letters {originalLetter} and {letterToSwap} have been swapped!");
+
+                        // Implement actual swapping with dictionary here...
+                        acceptedSwap = true;
+                    }
+
+                    
+                }
+            }
+
+
+
+        }
+    }
+}
---
+++ Enigma-Machine/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Enigma_Machine
+{
+    internal class Program
+
+
+
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enigma Machine started! Type your message for encryption:");
+            string Message = Console.ReadLine();
+            
+            Console.WriteLine("Do you wish to use the plugboard? y/n");
+            char plugUse = char.Parse(Console.ReadLine());
+            plugUse = char.ToLower(plugUse);
+
+            PlugBoard plugboard = null;
+            if (plugUse == 'y') {
+
+                Console.WriteLine("Board will be used.\n");
+                plugboard = new PlugBoard();
+                plugboard.SetupPlugboard();
+            }
+            else {
+                Console.WriteLine("Board will not be used\n");
+            }
+                
+                    
+            
+
+            Console.WriteLine($"{Message}");
+
+
+
+            Console.ReadLine();
+        }
+
+        static void Encrypt() { 
+        
+        }
+    }
+}
